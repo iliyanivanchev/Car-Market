374fb46897f18c12a1f31aec6abe8b168dc7bf35
Iliyan Ivanchev <157714078+iliyanivanchev@users.noreply.github.com>
2024-08-16 14:30:43 +0300

    Edit and Delete buttons ownership rendering
    Showing Edit and Delete buttons only for owner

--- client/src/components/car-details/CarDetails.jsx
+++ client/src/components/car-details/CarDetails.jsx
@@ -3,11 +3,16 @@ import styles from "./CarDetails.module.css";
 import { Link, useParams } from "react-router-dom";
 
 import { useGetOne } from "../../hooks/useCars";
+import { useContext } from "react";
+import { AuthContext } from "../../contexts/AuthContexts";
 
 
 export default function CarDetails() {
+    const { userId } = useContext(AuthContext);
     const { carId } = useParams();
     const [car] = useGetOne(carId);
+
+    const isOwner = userId === car._ownerId;
 
     return (
         <section className={styles["listing-details"]}>
@@ -24,10 +29,12 @@ export default function CarDetails() {
 
                 <p className={styles["description-para"]}>{car.description}</p>
 
-                <div className={styles["listings-buttons"]}>
-                    <Link to={`/car-edit/${car._id}`} className={styles["button-list"]}>Edit</Link>
-                    <Link to="#" className={styles["button-list"]}>Delete</Link>
-                </div>
+                {isOwner && (
+                    <div className={styles["listings-buttons"]}>
+                        <Link to={`/car-edit/${car._id}`} className={styles["button-list"]}>Edit</Link>
+                        <Link to="#" className={styles["button-list"]}>Delete</Link>
+                    </div>
+                )}
             </div>
         </section>
     );
