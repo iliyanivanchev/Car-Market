c2952035f023d091123e03da37706a7ca93a9dbf
Iliyan Ivanchev <157714078+iliyanivanchev@users.noreply.github.com>
2024-08-16 23:01:23 +0300

    Refactoring CarDelete html and css

--- client/src/components/car-delete/CarDelete.jsx
+++ client/src/components/car-delete/CarDelete.jsx
@@ -4,9 +4,11 @@ import { useState } from "react";
 import { Link, useNavigate, useParams } from "react-router-dom";
 
 import { remove } from "../../api/car-api";
+import { useGetOne } from "../../hooks/useCars";
 
 export default function CarDelete() {
     const { carId } = useParams();
+    const [car] = useGetOne(carId);
     const [error, setError] = useState('');
     const navigate = useNavigate();
 
@@ -21,17 +23,24 @@ export default function CarDelete() {
     }
 
     return (
-        <div className={styles["container"]}>
+        <section className={styles["listing-details"]}>
+            <div className={styles["listing"]}>
+                <div className={styles["preview"]}>
+                    <img className={styles["preview-img"]} src={car.imageUrl} />
+                </div>
+                <h2 className={styles["listing-h2"]}>{car.brand} {car.model}</h2>
+                <div>
+                    <div className="data-info">
+                        <h3 className={styles["data-info-h3"]}>Year: {car.year}</h3>
+                        <h3 className={styles["data-info-h3"]}>Price: {car.price} $</h3>
+                    </div>
+                </div>
+            </div>
             <h2 className={styles["container-P"]}>Are you sure you wont to delete this listing?</h2>
-            {error &&
-                (
-                    <span className={styles["error"]}>{error}</span>
-                )}
-            <hr className={styles["hr"]} />
             <div className={styles["btn-section"]}>
                 <button className={styles["confirm-btn"]} onClick={deleteHandler}>Confirm</button>
                 <Link to={`/car-details/${carId}`} className={styles["cancel-btn"]}>Cancel</Link>
             </div>
-        </div>
+        </section>
     );
 }
