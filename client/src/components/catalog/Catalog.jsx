3bdf01ec276a13c7f42fad688ee1bf987c709ae8
Iliyan Ivanchev <157714078+iliyanivanchev@users.noreply.github.com>
2024-08-16 22:31:31 +0300

    Add Back to Home button in Catalog component

--- client/src/components/catalog/Catalog.jsx
+++ client/src/components/catalog/Catalog.jsx
@@ -2,6 +2,7 @@ import styles from "./Catalog.module.css";
 
 import SingleCar from "./single-car/SingleCar";
 import { useGetCars } from "../../hooks/useCars";
+import { Link } from "react-router-dom";
 
 export default function Catalog() {
     const [cars] = useGetCars()
@@ -15,6 +16,9 @@ export default function Catalog() {
                         ? cars.map(car => <SingleCar key={car._id} car={car} />)
                         : <p className={styles["no-cars"]}>No cars in database.</p>
                 }
+            </div>
+            <div className={styles["btn-section"]}>
+                <Link to="/" className={styles["button"]}>Back to Home</Link>
             </div>
         </section>
     );
